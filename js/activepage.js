ce7fe2f31a29ad6ffb944ec7a3c56d24ed8dee6d
awisense <110358965+awisense@users.noreply.github.com>
2024-06-22 18:44:31 +0300

    Delete js directory

--- js/activepage.js
+++
@@ -1,8 +0,0 @@
-let links = document.querySelectorAll(".nav-link");
-let bodyId = document.querySelector("body").id;
- 
-for(let link of links){
-    if(link.dataset.active == bodyId){
-        link.classList.add("active");
-    }
-}
